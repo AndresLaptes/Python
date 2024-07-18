a3195a839aa8e15d7e861a731a8cdc3d70116d3e
Andres Lucian Laptes Costan <alaptes.210404@gmail.com>
2024-07-18 17:55:45 +0200

    Bueno... Nada

---
+++ proyectosRandoms/VoiceRecorder/VoiceRecorder.py
@@ -0,0 +1,9 @@
+#What the program does? 
+#- Your default mic starts recording
+#- The recording is saved in the same locatoin of source-code
+#- After custorm time the program exits
+
+import keyboard as k
+
+k.wait('esc')
+print("esc pulsado")
